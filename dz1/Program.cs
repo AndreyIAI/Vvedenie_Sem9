1cc5edb1ba6a01970311f2e75101cc4459e3fa41
IAIvanoff <autokitsto23@gmail.com>
2022-12-05 22:31:41 +0300

    Домашняя работа к 9 семинару

---
+++ dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+System.Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int m = 1;
+string PrintNumber (int start, int end)
+{
+    if (start==end) return start.ToString();
+    return (start + " " +PrintNumber (start + 1, end));
+}
+int SumNumber (int start, int end)
+{
+    if(start == end) return start;
+return end+ SumNumber(start,end-1);
+}
+System.Console.WriteLine(PrintNumber(m,n));
